4607b14485597b2abbc4615b08a47b8e6829cee4
Shindo <cn.shindo@gmail.com>
2017-04-04 16:28:24 -0700

    make the parser runnable

--- Typecheck.java
+++ Typecheck.java
@@ -1,19 +1,14 @@
+public class Typecheck {
+    public static void main(String[] args) throws ParseException {
+		// According to the instruction: "java Typecheck < P.java"
+        // We use `System.in` as the input stream.
+        try {
+            new MiniJavaParser(System.in);
+            MiniJavaParser.Goal();
 
-public class Typecheck
-{
-	public static void main(String[] args)
-	{
-		//just getting started
-		if(args.length > 0)
-		{
-			//args[0] is the file.. use parser to interact with it?
-
-			//just checking if java compiles and stuff, basically at "hello world" stage right now.
-			System.out.println(args[0]);
-		}
-		else
-		{
-			System.out.println("Error, no file to typecheck.");
-		}
+            System.out.println("Program type checked successfully");
+        } catch (ParseException e) {
+            System.out.println("Type error");
+        }
 	}
 }
