7cd0aee5001700925e152644815519d0a1b530bb
Shindo <cn.shindo@gmail.com>
2017-04-04 17:08:50 -0700

    apply DepthFirstVisitor to parser goal.

--- Typecheck.java
+++ Typecheck.java
@@ -1,10 +1,13 @@
+import syntaxtree.*;
+import visitor.*;
+
 public class Typecheck {
     public static void main(String[] args) throws ParseException {
         // According to the instruction: "java Typecheck < P.java"
         // We use `System.in` as the input stream.
         try {
             new MiniJavaParser(System.in);
-            MiniJavaParser.Goal();
+            MiniJavaParser.Goal().accept(new DepthFirstVisitor());
 
             System.out.println("Program type checked successfully");
         } catch (ParseException e) {
